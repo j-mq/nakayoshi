0af8a66d549cb5f9d07179ac16a77b09af07a5b0
Jacobo <jacobo@fixel.co.jp>
2023-04-14 17:03:59 +0900

    importing fonts and icons

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,28 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang='en'>
+      <Head>
+        <link rel='preconnect' href='https://fonts.googleapis.com' />
+        <link
+          rel='preconnect'
+          href='https://fonts.gstatic.com'
+          cross-origin='true'
+        />
+        <link
+          href='https://fonts.googleapis.com/css2?family=Dongle&family=Noto+Sans+JP&display=swap'
+          rel='stylesheet'
+        />
+        <link
+          rel='stylesheet'
+          href='https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@40,700,1,0&display=swap'
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
